6d9d4582ced3245136334908fea6be8def2ab374
Temitayo Jimoh <temi.boy@MacBook-Pro-3.local>
2021-05-01 14:05:25 -0700

    added checkboxes and styling to modal

---
+++ assets/javascript.js
@@ -0,0 +1,13 @@
+// modal
+const submitButton = document.querySelector("#submit");
+const modalBg = document.querySelector(".modal-background");
+const modal = document.querySelector(".modal");
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
