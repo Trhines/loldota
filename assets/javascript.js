8393f8588f42023c406f138ee2afec2f52851200
Temitayo Jimoh <temi.boy@MacBook-Pro-3.local>
2021-05-04 15:06:46 -0700

    No changes

--- assets/javascript.js
+++ assets/javascript.js
@@ -13,29 +13,30 @@ modalBg.addEventListener('click', () => {
     modal.classList.remove('is-active');
 })
 
+function 
 function printStats(profileName, leaderBoard, MMR, SoloComp, compRank, gamesWon, gamesLost, winRate) {
 
 }
 //replace deyy with a variable for search input
-  fetch('https://fortnite-api.com/v1/stats/br/v2?name=deyy')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    var name = data.data.account.name
-    var deaths = data.data.stats.all.overall.deaths
-    var kd = data.data.stats.all.overall.kd
-    var kills = data.data.stats.all.overall.kills
-    var score = data.data.stats.all.overall.score
-    var scorePerMatch = data.data.stats.all.overall.scorePerMatch
-    var playersOutLived = data.data.stats.all.overall.playersOutlived
-    var top3 = data.data.stats.all.overall.top3
-    var top5 = data.data.stats.all.overall.top5
-    var top10 = data.data.stats.all.overall.top10
-    var top25 = data.data.stats.all.overall.top25
-    var winrate = data.data.stats.all.overall.winrate
-    var wins = data.data.stats.all.overall.wins
-  });
+fetch('https://fortnite-api.com/v1/stats/br/v2?name=deyy')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var name = data.data.account.name
+        var deaths = data.data.stats.all.overall.deaths
+        var kd = data.data.stats.all.overall.kd
+        var kills = data.data.stats.all.overall.kills
+        var score = data.data.stats.all.overall.score
+        var scorePerMatch = data.data.stats.all.overall.scorePerMatch
+        var playersOutLived = data.data.stats.all.overall.playersOutlived
+        var top3 = data.data.stats.all.overall.top3
+        var top5 = data.data.stats.all.overall.top5
+        var top10 = data.data.stats.all.overall.top10
+        var top25 = data.data.stats.all.overall.top25
+        var winrate = data.data.stats.all.overall.winrate
+        var wins = data.data.stats.all.overall.wins
+    });
 
 
 var userName = "marxe"
@@ -43,29 +44,32 @@ var region = "na"
 var url = ('https://ow-api.com/v1/stats/pc/us/Snapshot-11568/complete')
 
 fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    
-    console.log(data)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
 
-    var compCard = data.competitiveStats.awards.cards
-    var compMedals = data.competitiveStats.awards.medals
-    var compBronze = data.competitiveStats.awards.medalsBronze
-    var compSilver = data.competitiveStats.awards.medalsSilver
-    var compGold = data.competitiveStats.awards.medalsGold
-    var compGamesPlayed = data.competitiveStats.games.played
-    var compGamesWon = data.competitiveStats.games.won
+        console.log(data)
 
-    var quickCards = data.quickPlayStats.awards.cards
-    var quickMedals = data.quickPlayStats.awards.medals
-    var quickBronze = data.quickPlayStats.awards.medalsBronze
-    var quickSilver = data.quickPlayStats.awards.medalsSilver
-    var quickGold = data.quickPlayStats.awards.medalsGold
-    var quickGamesPlayed = data.quickPlayStats.games.played
-    var quickGamesWon = data.quickPlayStats.games.won
+        var compCard = data.competitiveStats.awards.cards
+        var compMedals = data.competitiveStats.awards.medals
+        var compBronze = data.competitiveStats.awards.medalsBronze
+        var compSilver = data.competitiveStats.awards.medalsSilver
+        var compGold = data.competitiveStats.awards.medalsGold
+        var compGamesPlayed = data.competitiveStats.games.played
+        var compGamesWon = data.competitiveStats.games.won
 
-  });
+        var quickCards = data.quickPlayStats.awards.cards
+        var quickMedals = data.quickPlayStats.awards.medals
+        var quickBronze = data.quickPlayStats.awards.medalsBronze
+        var quickSilver = data.quickPlayStats.awards.medalsSilver
+        var quickGold = data.quickPlayStats.awards.medalsGold
+        var quickGamesPlayed = data.quickPlayStats.games.played
+        var quickGamesWon = data.quickPlayStats.games.won
+
+    });
 
 
+function searchInput(searchInput) {
+
+}
