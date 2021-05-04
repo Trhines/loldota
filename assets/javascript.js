4c5fdac9115ed85c50449a9d366345829c407e9b
Trenton Rhines <trhines94@gmail.com>
2021-05-03 21:29:25 -0700

    added dynamic searches

--- assets/javascript.js
+++ assets/javascript.js
@@ -23,21 +23,19 @@ function printStats(profileName, leaderBoard, MMR, SoloComp, compRank, gamesWon,
 
 //requestUrl = 'https://api.opendota.com/api/players/105248644?api_key=6eb1beea-905d-4e5d-96b4-e90090e00a0a'
 
-//example stat calls---------------------------------------------------------------------------
-fetch('https://api.opendota.com/api/players/105248644?api_key=6eb1beea-905d-4e5d-96b4-e90090e00a0a')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+// fetch('https://api.opendota.com/api/players/105248644?api_key=6eb1beea-905d-4e5d-96b4-e90090e00a0a')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
 
-   // for (i = 0; i < data.length)
-    console.log(data.profile.name)
-    console.log(data.leaderboard_rank)
-    console.log(data.mmr_estimate.estimate)
-    console.log(data.solo_competitive_rank)
-    console.log(data.competitive_rank)
-  });
+//     console.log(data.profile.name)
+//     console.log(data.leaderboard_rank)
+//     console.log(data.mmr_estimate.estimate)
+//     console.log(data.solo_competitive_rank)
+//     console.log(data.competitive_rank)
+//   });
 
 
   
@@ -51,7 +49,6 @@ fetch('https://api.opendota.com/api/players/105248644?api_key=6eb1beea-905d-4e5d
 //   });
   //-------------------------------------------------------------------------------------------
 
-  
 
 function searchProPlayers(searchInput){
     fetch('https://api.opendota.com/api/proPlayers?api_key=6eb1beea-905d-4e5d-96b4-e90090e00a0a')
@@ -60,10 +57,29 @@ function searchProPlayers(searchInput){
     })
     .then(function (data) {
       console.log(data)
-          for( i=0; i<1000; i++){
+          for( i=0; i<data.length; i++){
             console.log(searchInput)
               if(data[i].name === searchInput){
+
+
                   console.log('hit')
+                  console.log(data[i].account_id)
+
+
+                  fetch('https://api.opendota.com/api/players/' + data[i].account_id + '?api_key=6eb1beea-905d-4e5d-96b4-e90090e00a0a')
+                    .then(function (response) {
+                      return response.json();
+                    })
+                    .then(function (data) {
+                      console.log(data);
+
+                      console.log(data.profile.name)
+                      console.log(data.leaderboard_rank)
+                      console.log(data.mmr_estimate.estimate)
+                      console.log(data.solo_competitive_rank)
+                      console.log(data.competitive_rank)
+                    });
+
                   break
               }
           }      
